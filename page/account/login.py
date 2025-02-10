16b6b6e0897946b7317aab47dd5768038ad26c6b
chrisbrunet <132419309+chrisbrunet@users.noreply.github.com>
2025-02-09 22:17:53 -0700

    updated strava login button

--- page/account/login.py
+++ page/account/login.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import utils.auth as auth
+import base64
 
 if "refresh_token" not in st.session_state:
     st.session_state.refresh_token = None
 if "access_token" not in st.session_state:
     st.session_state.access_token = None
+
+def get_base64_image(image_path):
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode()
+
+image_base64 = get_base64_image("assets/strava_icon.png")
 
 client_id = st.secrets.CLIENT_ID
 client_secret = st.secrets.CLIENT_SECRET
@@ -16,7 +23,13 @@ st.header("Welcome to the Strava Data Analyzer!")
 
 st.write("This web app can connect to your Strava account and display all of you activity data. To use this app, please authorize Strava access by clicking the button below:")
 
-st.link_button("Login with Strava 🏃", auth_url)
+# st.link_button("Login with Strava 🏃", auth_url)
+html = f"""
+<a href="{auth_url}">
+    <img src="data:image/png;base64,{image_base64}" width="200">
+</a>
+"""
+st.markdown(html, unsafe_allow_html=True)
 
 try:
     st.session_state.refresh_token = st.query_params.code
