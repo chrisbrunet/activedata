132f415154af2ea6b3199eeb26f58cd1649e36ba
chrisbrunet <132419309+chrisbrunet@users.noreply.github.com>
2025-02-24 18:52:42 -0700

    finally fixed typo

--- page/account/login.py
+++ page/account/login.py
@@ -19,7 +19,7 @@ app_url = st.secrets.APP_URL
 
 auth_url = auth.get_authorization_url(app_url, client_id)
 
-st.header("Welcome to the ActiveData!")
+st.header("Welcome to ActiveData!")
 
 st.write("This web app can connect to your Strava account and display your activity data through various visualizations. To use this app, please authorize Strava access by clicking the button below:")
 
